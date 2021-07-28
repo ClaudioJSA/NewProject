c9f65663d8f5b1597fe521b8ec9e50925f0e413a
Cláudio Juliano <claudio11jsa@gmail.com>
2021-07-28 19:52:16 -0300

    Teste 1

---
+++ Hello/src/main/java/br/edu/ifnmg/poo/hello/Program.java
@@ -0,0 +1,13 @@
+package br.edu.ifnmg.poo.hello;
+
+/**
+ * Classe para o primeiro programa.
+ * @author Claudio Alcantara
+ * @version 0.1
+ */
+public class Program {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
